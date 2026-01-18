12a15b57c6328ed9f70aa75afb1dac2352ba2fbe
victor071997 <himanojkumar747@gmail.com>
2026-01-18 20:37:41 +0530

    jenkins shell python

---
+++ jen.py
@@ -0,0 +1,25 @@
+#1
+print (10*2)
+print(10%2)
+
+#2
+length_of_rect=10
+length_of_circle=2
+print (length_of_rect * length_of_circle)
+print(f'area of something {length_of_rect * length_of_circle}')
+
+#3 (if want to know the 2^5 power values)
+print(2**2) #4
+print(3**3) #27
+
+#4
+#if you integer value after division use '//'
+#if you use '/' it will show ture value like decimal valeus
+print(12/5) #2.4
+print(12//5) #2
+
+#5
+result = 2*(3+4)**2/5-6
+print(result)
+#it will work on BODMAS rule
+#(BODMAS) (brackets, order of powers or roots, division, multiplication, addition, substraction)
